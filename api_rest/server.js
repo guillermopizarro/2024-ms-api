2b6f8f1b3936d3cfb90b6bec81552513a15c8b36
guillermopizarro <gpizarro@espol.edu.ec>
2024-10-05 12:27:53 -0500

    GET, POST

--- api_rest/server.js
+++ api_rest/server.js
@@ -3,6 +3,7 @@ const body_parser = require('body-parser')
 
 const config = require('./config')
 const db = require('./db')
+const routes = require('./network/routes')
 
 var app = express()
 
@@ -10,6 +11,8 @@ db(config.DB_URL)
 
 app.use( body_parser.json() )
 app.use( body_parser.urlencoded({extended:false}) )
+
+routes(app)
 
 app.listen( config.PORT )
 console.log(`La aplicacion se encuentra arriba en http://localhost:${config.PORT}/`)
