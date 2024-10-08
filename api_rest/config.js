d7645bc4de3d296367dbbca669fd72eb3511a47b
Giovanny Jácome <gio_jacome@hotmail.com>
2024-10-07 22:19:33 -0500

    actualizacion en cade de conexion

--- api_rest/config.js
+++ api_rest/config.js
@@ -1,5 +1,6 @@
 const config = {
     PORT: 3000,
-    DB_URL: 'mongodb://dba:12345@localhost:27017/miapp?authSource=admin'
+    DB_URL: 'mongodb://dba:12345@dba:27017/miapp?authSource=admin'
 }
+
 module.exports = config
