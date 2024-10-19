f2ca541200970eed3f3dfc39fd8e50bde0927e4d
guillermopizarro <gpizarro@espol.edu.ec>
2024-10-18 21:43:05 -0500

    Implementación de RPC

---
+++ api_rpc/client/main.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const client = require('./index');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    client.getUsers({}, (error, data) => {
+        if (!error) {
+            console.log(data);
+            res.send(data);
+        } else {
+            console.error(error);
+            res.status(500).send({ msg: error.message });
+        }
+    });
+});
+
+app.post('/', (req, res) => {
+    const user = req.body;
+    client.addUser(user, (error, data) => {
+        if (!error) {
+            console.log(data);
+            res.send({ msg: 'Data added successfully.', user: data });
+        } else {
+            console.error(error);
+            res.status(500).send({ msg: error.message });
+        }
+    });
+});
+
+
+app.listen(5555, () => {
+    console.log('Server started on 5555.');
+});
